f04bd2b75e81ba2a25cf4cbcbf1f8e240f538cca
psahanap <36263701+psahanap@users.noreply.github.com>
2018-02-14 11:06:19 +0530

    commit

--- sgstack.sh
+++ sgstack.sh
@@ -8,6 +8,5 @@ then
 /usr/local/bin/aws cloudformation update-stack --stack-name test-SG-creation --template-body file:///opt/jenkins/subbranch/test/sg\ template.json.txt --parameters file:///opt/jenkins/subbranch/test/parameters.json --region ap-south-1;
 else
 /usr/local/bin/aws cloudformation create-stack --stack-name test-SG-creation --template-body file:///opt/jenkins/subbranch/test/sg\ template.json.txt --parameters file:///opt/jenkins/subbranch/test/parameters.json --region ap-south-1;
-fi
 /usr/local/bin/aws cloudformation describe-stack-events --stack-name test-SG-creation --region ap-south-1 | grep -iE 'ResourceStatus|ResourceType|LogicalResourceId';
-exit
+fi
