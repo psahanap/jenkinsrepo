fed0ad7ac937230069474826fb717be8bbe43949
psahanap <36263701+psahanap@users.noreply.github.com>
2018-02-15 15:11:22 +0530

    commit-no-update

--- vpcupdate.sh
+++ vpcupdate.sh
@@ -18,10 +18,16 @@ if [ $status -eq 0 ]
 					/usr/local/bin/aws cloudformation describe-stack-events --stack-name test-vpc-creation --region us-east-2 | grep -iE 'ResourceStatus|ResourceType|LogicalResourceId|PhysicalResourceId'
 					sleep 3s
 					else
-					if [ "$nooflines" -ge 2 ] && [ "$nooflines" -eq 0 ];
+					if [ "$nooflines" -ge 2 ] ;
 					then
 					echo "Stack Creation Completed"
 					break
+					else
+					if [ "$nooflines" -eq 0 ] ;
+					then
+					echo "(ValidationError) when calling the UpdateStack operation: No updates are to be performed."
+					break
+					fi
 					fi
 					fi
 					done
