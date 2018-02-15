a9f38ff194769b7acf24c496df530752033690fc
psahanap <36263701+psahanap@users.noreply.github.com>
2018-02-15 11:33:56 +0530

    commit

---
+++ vpcstack.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#source ~/.bash_profile
+/usr/local/bin/aws cloudformation describe-stacks --stack-name test-vpc-creation --region us-east-2
+status=`echo $?`
+echo $status
+if [ $status -eq 0 ]
+then
+/usr/local/bin/aws cloudformation update-stack --stack-name test-vpc-creation --template-body file:///opt/jenkins/subbranch/test/vpc\ template.json.txt --parameters file:///opt/jenkins/subbranch/test/parameters.json --region us-east-2 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+else
+/usr/local/bin/aws cloudformation create-stack --stack-name test-vpc-creation --template-body file:///opt/jenkins/subbranch/test/vpc\ template.json.txt --parameters file:///opt/jenkins/subbranch/test/parameters.json --region us-east-2 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+fi
+while true
+do
+/usr/local/bin/aws cloudformation describe-stack-events --stack-name test-vpc-creation --region us-east-2 |  grep -iE 'ResourceStatus|ResourceType' |sed 's|[",:]||g' | grep -E 'ResourceType.*AWSCloudFormationStack' > lines
+cat lines
+nooflines=`cat lines | wc -l`
+echo "The Loop will continue until stack is completed/updated."
+if [ "$nooflines" -ne 2 ];
+then
+echo "Stack Creation is in Progress,Below are the events happening now"
+/usr/local/bin/aws cloudformation describe-stack-events --stack-name test-vpc-creation --region us-east-2 | grep -iE 'ResourceStatus|ResourceType|LogicalResourceId|PhysicalResourceId'
+else
+if [ "$nooflines" -eq 2 ];
+then
+echo "Stack Creation Completed"
+break
+fi
+fi
+done
+while true
+do
+/usr/local/bin/aws cloudformation describe-stack-resources --stack-name test-vpc-creation --region us-east-2 | grep -iE 'ResourceStatus|ResourceType' |sed 's|[",:]||g' |grep -E 'ResourceStatus.*_COMPLETE|ResourceType.*'
+rsatus=`echo $?`
+echo $rstatus
+if [ $rstatus -ne 0 ];
+then
+echo "Resources are Created Successfully, below are the details"
+/usr/local/bin/aws cloudformation describe-stack-resources --stack-name test-vpc-creation --region us-east-2 | grep -iE 'ResourceStatus|ResourceType|LogicalResourceId'
+else
+break
+fi
+done
